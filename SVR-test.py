e85abc4209fcc7271621a68378d52f206fb63e9e
luoleiming <luoleiming@yingu.com>
2019-08-08 17:27:49 +0800

    test svc

---
+++ SVR-test.py
@@ -0,0 +1,32 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+
+loadData = np.loadtxt("./testData.txt", skiprows=0)
+dataLength = len(loadData[0])
+dataY = loadData[:, dataLength - 1]
+dataX = loadData[:, 0:dataLength - 1]
+
+xTrainData, xTestData, yTrainData, yTestData = train_test_split(dataX, dataY, test_size=0.5, random_state=0)
+
+print(len(xTrainData))
+# scX = StandardScaler()
+# xTrainData = scX.fit_transform(xTrainData)
+# xTestData = scX.fit_transform(xTestData)
+
+clf1 = SVR(kernel='poly')
+clf1.fit(xTrainData, yTrainData)
+yPredict1 = clf1.predict(xTestData)
+
+print(yPredict1)
+
+k = 0
+for i in range(len(yTestData)):
+    if (yPredict1[i] - yTestData[i])<0.10:
+        k += 1
+print(k)
+
+accuracySvm = float(k) / float(len(yTestData))
+print(accuracySvm)
+exit(0)
---
+++ numpyTest.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# b = a[1:3, 1:3]
+# c = a[1:3, [1, 2]]
+# d = a[..., 1:]
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+arr = np.arange(6).reshape(2, 3)
+print(arr)
+
+exit(0)
