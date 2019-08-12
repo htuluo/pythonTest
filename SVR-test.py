c9e3291442ab0204de38176000186baf84f9cf45
luoleiming <luoleiming@yingu.com>
2019-08-12 15:18:50 +0800

    preprocessing one_hot labelencoder

--- SVR-test.py
+++ SVR-test.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 import time
 
 loadData = np.loadtxt("./testData.txt", skiprows=0)
@@ -21,15 +22,25 @@ scX = StandardScaler()
 xTrainData = scX.fit_transform(xTrainData)
 xTestData = scX.fit_transform(xTestData)
 
+scY = StandardScaler()
+print("yTrainData Before:", yTrainData)
+yTrainData = yTrainData.reshape(-1, 1)
+yTestData = yTestData.reshape(-1, 1)
+print("yTrainData After:", yTrainData.reshape(1, -1))
+print("yTrainData After:", yTrainData.reshape(1, -1))
 clf1 = SVR(kernel='poly')
 clf1.fit(xTrainData, yTrainData)
+# score = clf1.score(xTestData,yTestData,)
+# print(score)
 yPredict1 = clf1.predict(xTestData)
 
-print(yPredict1)
+print("预测值：", yPredict1)
+print("均方误差：", mean_squared_error(yTestData, yPredict1))
+print("绝对均方误差：", mean_absolute_error(scY.inverse_transform(yTestData), scY.inverse_transform(yPredict1)))
 
 k = 0
 for i in range(len(yTestData)):
-    if (yPredict1[i] - yTestData[i]) < 0.10:
+    if (yPredict1[i]) > 0.50:
         k += 1
 print(k)
 
--- numpyTest.py
+++ numpyTest.py
@@ -1,4 +1,26 @@
 import numpy as np
+from sklearn import preprocessing
+
+array = np.array([[1.1, 2.2, 5.5]])
+normalize = preprocessing.normalize(array)
+print(normalize)
+randint = np.zeros((1,3))
+print("pre",np.linalg.norm(array))
+for col in range(3):
+    randint[0][col] = array[0][col] / np.linalg.norm(array[0])
+print(randint)
+
+#one_hot demo
+# one_hot_encoder = preprocessing.OneHotEncoder()
+# one_hot_encoder.fit([[0,1],[1,2],[1,3],[0,1]])
+# transform__toarray = one_hot_encoder.transform([[0, 1]]).toarray()
+# print("Transform:",transform__toarray)
+
+
+label_encoder = preprocessing.LabelEncoder()
+fit = label_encoder.fit([1, 222, 44, 228, 878])
+fit_transform = fit.transform([1, 222])
+print("FIT:",fit_transform)
 
 # a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 # b = a[1:3, 1:3]
@@ -9,12 +31,14 @@ import numpy as np
 # print(c)
 # print(d)
 
-arr = np.arange(24).reshape(4, 6)
-print(arr)
-print(arr[::2])
-print(arr[2::])
-print(arr[-1:])
-print(arr[:-1])
-print(arr[::-1])
+# arr = np.arange(24).reshape(4, 6)
+# print(arr)
+# print(np.array([1,2,3,4]).reshape(1,-1))
+# print(np.array([1,2,3,4]).reshape(-1,1))
+# print(arr[::2])
+# print(arr[2::])
+# print(arr[-1:])
+# print(arr[:-1])
+# print(arr[::-1])
 
 exit(0)
